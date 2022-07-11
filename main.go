deccb321e4c727871296a2a3fda87a8b128dfb6e
root <santhosh.fernandes@gmail.com>
2022-07-11 16:13:05 +0000

    storing secret key in mem

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 )
 
-var secretkey = "l3afd"
-
 func GenerateJWT(hostname, role string) (string, error) {
+
+	secretkey := os.Getenv("L3AFD_SEC_KEY")
 	var mySigningKey = []byte(secretkey)
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
@@ -35,8 +35,12 @@ func ValidateJWT(tknStr string) bool {
 	// Initialize a new instance of `Claims`
 	claims := &jwt.MapClaims{}
 
+	secretkey := os.Getenv("L3AFD_SEC_KEY")
+
+	fmt.Println("Secret key", secretkey)
+
 	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(secretkey), nil
+		return []byte(secretkey1), nil
 	})
 
 	if err != nil {
@@ -62,16 +66,17 @@ func ValidateJWT(tknStr string) bool {
 }
 
 func main() {
-	machineHostname, err := os.Hostname()
-	if err != nil {
-		fmt.Println("hostname failed")
-	}
-	role := "admin"
-	tknStr, err := GenerateJWT(machineHostname, role)
-	if err != nil {
-		fmt.Println("token failed")
-	}
+	//machineHostname, err := os.Hostname()
+	//if err != nil {
+	//	fmt.Println("hostname failed")
+	//}
+	//role := "admin"
+	//tknStr, err := GenerateJWT(machineHostname, role)
+	//if err != nil {
+	//	fmt.Println("token failed")
+	//}
 
+	tknStr := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJob3N0bmFtZSI6ImwzYWYtbG9jYWwtdGVzdCIsInJvbGUiOiJhZG1pbiJ9.GzC1bH5fQoPLDS1HxoT5i2PqHnuKcerxeAaSd0Dy7rc"
 	fmt.Println("token: ", tknStr)
 
 	fmt.Println("valid token : ", ValidateJWT(tknStr))
